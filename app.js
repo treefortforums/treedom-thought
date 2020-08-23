08e32fabe2cbc7e7e995a3ddafd62b275aa96e94
treefortforums <treefortforums@gmail.com>
2020-08-23 03:51:45 -0700

    Changed to Treefort Creators

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 //Mongo DB
 var mongoose = require('mongoose');
-var mongoDB =  process.env.MONGODB_URI || dev_db_url;
+var mongoDB =  process.env.MONGODB_URI;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
